59d850121d9bf4201730e17d7912450914ab0b02
TimilsinaBimal <timilsina.bml05@gmail.com>
2021-07-08 14:59:04 +0545

    Model for bounding box detection

---
+++ src/models/model.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from utils.config import *
+
+
+def model():
+    mobile_net_model = MobileNetV2(
+        include_top=False,
+        input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, CHANNELS))
+    model = tf.keras.models.Sequential()
+    model.add(mobile_net_model)
+    model.add(tf.keras.layers.GlobalAveragePooling2D())
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(1024, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.Dense(512, activation='relu'))
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dense(4, name='bounding_box_detector'))
+    return model
